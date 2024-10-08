a6cf52b9cd29b932b54a4dbe56f071009fcb99d6
SmenNeoris <140518960+smneoris@users.noreply.github.com>
2024-10-08 11:52:15 -0500

    Updated README.md

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// configure template paths for tailwindcss
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
